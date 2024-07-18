4bfbfb6100086682070a8f09d1a33ac6ef9b8ed8
MarvinWalls <marvinwalls777@gmail.com>
2024-07-18 17:01:39 -0400

    Fix Substack Articles link in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white py-2 shadow-md">
-      <nav className="container mx-auto">
-        <ul className="flex space-x-4 justify-center">
-          <li><a href="#home" className="hover:text-yellow-500">Home</a></li>
-          <li><a href="#about" className="hover:text-yellow-500">About</a></li>
-          <li><a href="#substack" className="hover:text-yellow-500">Substack Articles</a></li>
-          <li><a href="#contact" className="hover:text-yellow-500">Contact</a></li>
-        </ul>
-      </nav>
+    <header className="bg-gray-800 text-white">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <h1 className="text-2xl font-bold">My Portfolio</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li><a href="#home" className="hover:text-yellow-400">Home</a></li>
+            <li><a href="#about" className="hover:text-yellow-400">About</a></li>
+            <li><a href="#substack-articles" className="hover:text-yellow-400">Substack Articles</a></li> {/* Update this link */}
+            <li><a href="#contact" className="hover:text-yellow-400">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
