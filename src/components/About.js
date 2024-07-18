08b7687b844c7e90bad7b341f613f463bad3dc10
MarvinWalls <marvinwalls777@gmail.com>
2024-07-18 16:50:18 -0400

    Reduce padding and height of about section

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
           From the simple days of cleaning up after weddings, I went on to make pizzas, ride a trash truck, grind parts on an assembly line, deliver groceries, and drive for Uber. My journey then led to more technical roles, such as working IT Help Desk in college and managing a team of six while overseeing data for over 2 million customer sites for Stanley Security now Securitas.
         </p>
         <p className="text-lg leading-relaxed mt-4">
-          Today, I am currently building and streamlining processes for Heartland Dental, the largest DSO in the world. I create dashboards with SAP SAC, optimize workflows with VBA and SQL, and handle various other tasks as needed by the organization. In the future, I aim to continue honing my coding skills and potentially return to Purdue for a Master's in Artificial Intelligence.
+          Today, I am currently building and streamlining processes for Heartland Dental, the largest DSO in the world. I create dashboards with SAP SAC, optimize workflows with VBA and SQL, and handle various other tasks as needed by the organization. In the future, I aim to continue honing my coding skills and potentially returning to Purdue for a Master's in Artificial Intelligence.
         </p>
         <p className="text-lg leading-relaxed mt-4">
           As you can see, I have a healthy love for large language models (LLMs). Before today, I had never touched JavaScript, but within hours, I have built this comprehensive website with the help of Claude and ChatGPT.
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <img src={Headshot} alt="Marvin Walls" className="rounded-full w-32 h-32 mx-auto my-4 border-4 border-white shadow-lg" />
         <h1 className="text-4xl font-bold text-yellow-500 fade-in">Marvin Walls</h1>
         <p className="text-xl mt-4 fade-in">Purdue University Graduate in Finance and Management</p>
-        <p className="text-lg mt-2 fade-in">4 Years of Experience Data Management and Analytics</p>
+        <p className="text-lg mt-2 fade-in">4 Years of Experience in Data Management and Analytics</p>
       </div>
     </section>
   );
