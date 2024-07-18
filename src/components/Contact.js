46c101939ead68333b9919d2a3483a9cbb0fc266
MarvinWalls <marvinwalls777@gmail.com>
2024-07-18 16:29:31 -0400

    Fix LinkedIn icon import in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faPhone, faMapMarkerAlt, faLinkedin } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'; // Correct import for LinkedIn icon
 
 const Contact = () => {
   return (
