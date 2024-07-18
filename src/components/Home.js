b849543e5dea3fb7fa218fffdf03b720411ea7b8
MarvinWalls <marvinwalls777@gmail.com>
2024-07-18 15:33:58 -0400

    Add animations and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,13 @@ import Headshot from '../images/LinkedInHeadshot.jpg';
 
 const Home = () => {
   return (
-    <section id="home">
-      <h1 className="text-2xl font-bold">Welcome to My Portfolio</h1>
-      <img src={Headshot} alt="Marvin Walls" className="rounded-full w-32 h-32 mx-auto my-4" />
-      <p>I am a Purdue University graduate with dual majors in Finance and Management, complemented by a Certificate in Entrepreneurship. With 4 years of experience in data management and analytics, I have a proven track record of leveraging advanced tools and methodologies to optimize processes, enhance data visibility, and drive significant cost savings. My expertise spans SAP Analytics Cloud, SQL, VBA, and project management, underscoring my passion for harnessing data to unlock future possibilities.</p>
+    <section id="home" className="text-center py-20 bg-gradient-to-r from-gray-800 to-gray-900 text-white">
+      <div className="container mx-auto">
+        <img src={Headshot} alt="Marvin Walls" className="rounded-full w-32 h-32 mx-auto my-4 border-4 border-white" />
+        <h1 className="text-4xl font-bold">Marvin Walls</h1>
+        <p className="text-xl mt-4">Purdue University Graduate in Finance and Management</p>
+        <p className="text-lg mt-2">Specializing in Data Management and Analytics</p>
+      </div>
     </section>
   );
 }
