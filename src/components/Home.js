0b842f2c3cdf50eaba3adadfd69e783f7643830c
MarvinWalls <marvinwalls777@gmail.com>
2026-03-02 13:02:19 -0500

    text edits - header

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="container mx-auto">
         <img src={Headshot} alt="Marvin Walls" className="rounded-full w-48 h-48 mx-auto my-4 border-4 border-white shadow-lg" />
         <h1 className="text-4xl font-bold text-yellow-500 fade-in">Marvin Walls</h1>
-        <p className="text-xl mt-4 fade-in">SAP Analytics Cloud Architect | High-Concurrency Dashboard Architecture | Advanced SAC Scripting | Financial Systems Optimization & Data Governance<p>
+        <p className="text-xl mt-4 fade-in">SAP Analytics Cloud Architect | High-Concurrency Dashboard Architecture | Advanced SAC Scripting | Financial Systems Optimization & Data Governance</p>
         <p className="text-lg mt-2 fade-in">5 Years of Experience in Data Management and Analytics</p>
       </div>
     </section>
